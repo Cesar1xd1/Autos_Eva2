3e7a5fc28522f89f5d5f2f5874b946d915376604
Cesar1xd1 <cesar1xd1@gmail.com>
2024-05-24 14:01:12 -0600

    cambio en la base de datos

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import {connect} from 'mongoose';
 
 (async ()=>{
     try {
-        const db = await connect("mongodb+srv://cesar2xd2:hZNsi1Pt3CpWUsAU@cluster0.gfejevy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        const db = await connect("mongodb+srv://cesar1xd1:1234cesar1234@cluster0.jqkk9cf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log("BD conexion to ", db.connection.name);
     } catch (error) {
         console.error(error);
