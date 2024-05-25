22b3f996a1c4da312d5bf5f13eb144248b3e99b0
Cesar1xd1 <cesar1xd1@gmail.com>
2024-05-24 21:04:30 -0600

    se modifico los archivos de app y conexion

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import {connect} from 'mongoose';
 
 (async ()=>{
     try {
-        const db = await connect("mongodb+srv://cesar1xd1:1234cesar1234@cluster0.jqkk9cf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        const db = await connect("mongodb+srv://cesar2xd2:cesar2xd2@clusterxd.a5efmkr.mongodb.net/?retryWrites=true&w=majority&appName=ClusterXD");
         console.log("BD conexion to ", db.connection.name);
     } catch (error) {
         console.error(error);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from './app';
 import './db';
 
-app.listen(3000,'0.0.0.0');
-console.log("server en puerto", 3001);
+app.listen(3000);
+console.log("server en puerto", 3000);
